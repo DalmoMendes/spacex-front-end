c6a7743ea5ec620911a6310d6fa4909d1127a963
Dalmo Mendes <dalmosilvamendes@gmail.com>
2021-12-10 15:16:58 -0300

    :rocket: start project spacex front-end!

---
+++ js/owl/myjquery-wol.js
@@ -0,0 +1,29 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    autoplay:true,
+    animateOut: 'slideOutDown',
+    animateIn: 'flipInX',
+    items:1,
+    margin:30,
+    stagePadding:30,
+    smartSpeed:450,
+    margin:20,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        400:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        800:{
+            items:4
+        },
+        1000:{
+            items:5
+        }
+    }
+})
